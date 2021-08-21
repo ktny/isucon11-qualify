887b75241ad6d32680f51e7d7253c27784c11a16
ktny <katsunori.t10@gmail.com>
2021-08-21 12:18:43 +0900

    fix deploy.sh

--- deploy_app.sh
+++ deploy_app.sh
@@ -6,12 +6,22 @@ echo "start deploy app ${USER}"
 dest=/home/isucon/webapp/go/isucondition
 
 # GOOS=linux go build -o app src/isucon/app.go
-cd webapp/go
 go build -o isucondition
 
 for server in isu01; do
+    # deploy db
+    ssh -tq $server "sudo systemctl stop mariadb"
+    ssh -tq $server "sudo rm -f /var/log/mysql/mysql-slow.log"
+    ssh -tq $server "mysql -uisucon -pisucon -e'flush logs;' isucondition"
+    scp mysql.cnf $server:/etc/mysql/conf.d/mysql.cnf
+    scp webapp/sql/0_Schema.sql $server:$dest
+    scp webapp/sql/1_InitData.sql $server:$dest
+    ssh -tq $server "/home/isucon/webapp/sql/init.sh"
+    ssh -tq $server "sudo systemctl start mariadb"
+
+    # deploy app
     ssh -tq $server "sudo systemctl stop isucondition.go.service"
-    scp ./isucondition $server:$dest
+    scp webapp/go/isucondition $server:$dest
     ssh -tq $server "sudo systemctl start isucondition.go.service"
     ssh -tq $server "sudo systemctl restart jiaapi-mock.service"
 done
--- deploy_db.sh
+++ deploy_db.sh
@@ -8,10 +8,10 @@ for server in isu01; do
     ssh -tq $server "sudo systemctl stop mariadb"
     ssh -tq $server "sudo rm -f /var/log/mysql/mysql-slow.log"
     ssh -tq $server "mysql -uisucon -pisucon -e'flush logs;' isucondition"
-    # ssh -tq $server "mysqladmin flush-logs"
     scp mysql.cnf $server:/etc/mysql/conf.d/mysql.cnf
     scp webapp/sql/0_Schema.sql $server:$dest
     scp webapp/sql/1_InitData.sql $server:$dest
+    ssh -tq $server "/home/isucon/webapp/sql/init.sh"
     ssh -tq $server "sudo systemctl start mariadb"
 done
 
