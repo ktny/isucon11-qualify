34ae26af09ba25ce2237a142521e4918d542339a
ktny <katsunori.t10@gmail.com>
2021-08-21 11:14:56 +0900

    fix

--- deploy_db.sh
+++ deploy_db.sh
@@ -4,13 +4,14 @@ echo "start deploy db ${USER}"
 
 dest=/home/isucon/webapp/sql/
 
-for server in isu01; do
+for server in isu01 isu02 isu03; do
     ssh -tq $server "sudo systemctl stop mariadb"
+    ssh -tq $server "sudo rm -f /var/log/mysql/*"
+    ssh -tq $server "mysql -uisucon -pisucon -e'flush logs;' isucondition"
+    # ssh -tq $server "mysqladmin flush-logs"
+    scp mysql.cnf $server:/etc/mysql/conf.d/mysql.cnf
     scp webapp/sql/0_Schema.sql $server:$dest
     scp webapp/sql/1_InitData.sql $server:$dest
-    scp mysql.cnf $server:/etc/mysql/mysql.cnf
-    ssh -tq $server "sudo rm -f /var/log/mysql/*"
-    ssh -tq $server "sudo mysql -uisucon -pisucon -e'flush logs;' isucondition"
     ssh -tq $server "sudo systemctl start mariadb"
 done
 
